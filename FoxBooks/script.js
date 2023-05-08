003cea79a22aa6f2e57db3cab165209edfeeeee4
bgulluni <98063882+bgulluni@users.noreply.github.com>
2023-05-08 02:40:12 -0400

    Update script.js

--- FoxBooks/script.js
+++ FoxBooks/script.js
@@ -1,68 +1,41 @@
-// Sample data object containing course and book information
+// JSON data (courses and books)
 const data = {
   "courses": [
     {
       "title": "Mathematics",
-      "book": "Introduction to Calculus",
-      "semester": "Fall 2023",
-      "professor": "Dr. Smith"
+      "book": "Introduction to Calculus"
     },
     {
       "title": "Physics",
-      "book": "Fundamentals of Physics",
-      "semester": "Spring 2024",
-      "professor": "Dr. Johnson"
+      "book": "Fundamentals of Physics"
     },
     {
       "title": "Biology",
-      "book": "Biology: A Global Approach",
-      "semester": "Fall 2023",
-      "professor": "Dr. Anderson"
+      "book": "Biology: A Global Approach"
     }
-    // Add more courses with semester and professor details as needed
-  ],
-  "books": [
-    {
-      "title": "Introduction to Calculus",
-      "isbn": "9781234567890",
-      "author": "John Smith"
-    },
-    {
-      "title": "Fundamentals of Physics",
-      "isbn": "9780987654321",
-      "author": "Jane Doe"
-    },
-    {
-      "title": "Biology: A Global Approach",
-      "isbn": "9785678901234",
-      "author": "David Johnson"
-    }
-    // Add more books with ISBN and author details as needed
+    // Add more courses and books as needed
   ]
 };
 
-// Display the course list on the courses.html page
-function display() {
+// Function to display all courses on Page 2
+function displayCourses() {
   const courseList = document.getElementById("courseList");
 
-  // Iterate through each course in the data object and create list items
-  data.courses.forEach(course => {
+  data.courses.forEach((course) => {
     const listItem = document.createElement("li");
-    listItem.textContent = course.title;
+    const link = document.createElement("a");
+    link.href = "book.html";
+    link.textContent = course.title;
+    listItem.appendChild(link);
+    courseList.appendChild(listItem);
 
-    // Add an event listener to navigate to the book.html page on click
-    listItem.addEventListener("click", function() {
+    // Store the course title and book title in sessionStorage for later retrieval
+    link.addEventListener("click", function () {
       sessionStorage.setItem("courseTitle", course.title);
       sessionStorage.setItem("bookTitle", course.book);
-      window.location.href = "book.html";
     });
-
-    // Append the list item to the course list
-    courseList.appendChild(listItem);
   });
 }
 
-// Call the display function when the courses.html page has finished loading
-document.addEventListener("DOMContentLoaded", function() {
-  display();
-});
+// Call the displayCourses function on page load
+displayCourses();
