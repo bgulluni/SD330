5b4b7d278ce8b243ac84282ccbadc1a8c905f945
bgulluni <98063882+bgulluni@users.noreply.github.com>
2023-05-08 01:27:56 -0400

    Update script.js

--- FoxBooks/script.js
+++ FoxBooks/script.js
@@ -13,6 +13,7 @@ const data = {
       "title": "Biology",
       "book": "Biology: A Global Approach"
     }
+    
   ]
 };
 
@@ -21,4 +22,32 @@ function displayCourses() {
   const courseList = document.getElementById("courseList");
 
   data.courses.forEach((course) => {
-    const listItem = document
+    const listItem = document.createElement("li");
+    const link = document.createElement("a");
+    link.href = "book.html";
+    link.textContent = course.title;
+    listItem.appendChild(link);
+    courseList.appendChild(listItem);
+
+    // Store the course title in a data attribute for later retrieval
+    link.dataset.courseTitle = course.title;
+    // Store the book title in a data attribute for later retrieval
+    link.dataset.bookTitle = course.book;
+
+    // Add an event listener to handle clicking on a course
+    link.addEventListener("click", handleCourseClick);
+  });
+}
+
+// Function to handle clicking on a course
+function handleCourseClick(event) {
+  const courseTitle = event.target.dataset.courseTitle;
+  const bookTitle = event.target.dataset.bookTitle;
+
+  // Store the course and book titles in localStorage for retrieval on the book page
+  localStorage.setItem("courseTitle", courseTitle);
+  localStorage.setItem("bookTitle", bookTitle);
+}
+
+// Call the displayCourses function on page load
+displayCourses();
